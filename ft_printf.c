f783c01faaeabfae4e3b663a619671bc70c706b4
Gabriel Aguiar De La Rocque <gde-la-r@c2r1s5.42porto.com>
2024-11-06 10:43:47 +0000

    Make file 1/2

---
+++ ft_printf.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 10:25:09 by gde-la-r          #+#    #+#             */
+/*   Updated: 2024/11/06 10:29:11 by gde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// %c Prints a single character.
+// %s Prints a string (as defined by the common C convention).
+// %p The void * pointer argument has to be printed in hexadecimal format.
+// %d Prints a decimal (base 10) number.
+// %i Prints an integer in base 10.
+// %u Prints an unsigned decimal (base 10) number.
+// %x Prints a number in hexadecimal (base 16) lowercase format.
+// %X Prints a number in hexadecimal (base 16) uppercase format.
+// %% Prints a percent sign.
+
+#include "libftprintf.h"
+
+int	ft_printf(const char *str, ...)
+{
+}
