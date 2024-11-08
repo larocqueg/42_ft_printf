ecbe047b18e4d1065f7fa2c3585af4c07a93c367
larocqueg <portugalgabriel2021@gmail.com>
2024-11-08 19:20:53 +0000

    ft_printf function done

--- ft_printf.c
+++ ft_printf.c
@@ -6,108 +6,28 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:21:48 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/08 18:36:58 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/11/08 19:10:46 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+/*
 #include <stdio.h>
-//#include "ft_printf.h"
-//#include "ft_strchr.c"
-//#include "ft_putchar.c"
-//#include "ft_putstr.c"
-//#include "ft_putpointer.c"
-
+#include "ft_printf.h"
+#include "ft_strchr.c"
+#include "ft_putchar.c"
+#include "ft_putstr.c"
+#include "ft_putpointer.c"
+int	ft_printf(const char *str, ...);
 static int	ft_putnbr(int nb);
+static int	ft_putnbr_neg(long n);
 static int	ft_type(const char *type, va_list argument);
-
-int	ft_printf(const char *str, ...)
-{
-	va_list	arguments;
-	int		i;
-
-	i = 0;
-	va_start(arguments, str);
-	while (*str)
-	{
-		if (*str == '%')
-		{
-			str++;
-			if (*str == '%')
-				i += ft_putchar('%');
-			else if (ft_strchr("cspdiuxX", *str))
-				i += ft_type(str, arguments);
-			else
-				i += ft_putchar(*str);
-		}
-		else
-		{
-			i += ft_putchar(*str);
-		}
-		str++;
-	}
-	va_end(arguments);
-	return (i);
-}
-
-static long	ft_isneg(long n)
-{
-	ft_putchar('-');
-	return (nb * -1);
-}
-
-static int	ft_putnbr(int nb)
-{
-	char	str[11];
-	int		i;
-	int		is_negative;
-	long	n;
-
-	n = nb;
-	is_negative = 0;
-	if (n == 0)
-		return (ft_putchar('0'));
-	if (n < 0)
-	{
-		is_negative = 1;
-		n = ft_isneg(n);
-	}
-	i = 0;
-	while (n > 0)
-	{
-		str[i] = n % 10 + '0';
-		n /= 10;
-		i++;
-	}
-	if (is_negative)
-		str[i++] = '-';
-	while (--i >= 0)
-		write(1, &str[i], 1);
-	return (i + 2 - is_negative);
-}
-
-static int	ft_type(const char *type, va_list argument)
-{
-	int	i;
-
-	i = 0;
-	if (*type == 'c')
-		i += ft_putchar(va_arg(argument, int));
-	else if (*type == 's')
-		i += ft_putstr(va_arg(argument, char *));
-	else if (*type == 'd' || *type == 'i')
-		i += ft_putnbr(va_arg(argument, int));
-	else if (*type == 'p')
-		i += ft_putpointer(va_arg(argument, char *));
-	return (i);
-}
 
 int	main(int ac, char **av)
 {
 	int	neg = -2147483648;
 	int	pos = 2147483647;
 	int	zero = 0;
-
 	ft_printf("Here is a char: %c\n", av[1][0]);
 	printf("printf(%c)\n", av[1][0]);
 	ft_printf("Here is a string: %s\n", av[1]);
@@ -124,5 +44,103 @@ int	main(int ac, char **av)
 	printf("printf(%i)\n", pos);
 	ft_printf("Here is the pointer of av[1]: %p\n", av[1]);
 	printf("printf(%p)\n", av[1]);
+	ft_printf("%%");
 	return (0);
+}
+*/
+int	ft_printf(const char *str, ...)
+{
+	va_list	arguments;
+	int		i;
+
+	i = 0;
+	va_start(arguments, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			if (ft_strchr("cspdiuxX", *str))
+				i += ft_type(str, arguments);
+			else
+				i += ft_putchar(*str);
+		}
+		else
+		{
+			i += ft_putchar(*str);
+		}
+		str++;
+	}
+	va_end(arguments);
+	return (i);
+}
+
+static int	ft_putnbr_neg(long n)
+{
+	char	str[11];
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	write(1, "-", 1);
+	n = -n;
+	if (n == 0)
+		return (ft_putchar('0'));
+	else
+	{
+		while (n > 0)
+		{
+			str[i++] = n % 10 + '0';
+			n /= 10;
+		}
+		while (--i >= 0)
+			write(1, &str[i], 1);
+		count = i + 1;
+	}
+	return (count + 1);
+}
+
+static int	ft_putnbr(int nb)
+{
+	char	str[11];
+	int		i;
+	int		count;
+	long	n;
+
+	n = nb;
+	count = 0;
+	if (n == 0)
+		return (ft_putchar('0'));
+	if (n < 0)
+		return (ft_putnbr_neg(n));
+	else
+	{
+		i = 0;
+		while (n > 0)
+		{
+			str[i++] = n % 10 + '0';
+			n /= 10;
+		}
+		while (--i >= 0)
+			write(1, &str[i], 1);
+		count = i;
+	}
+	return (count);
+}
+
+static int	ft_type(const char *type, va_list argument)
+{
+	int	i;
+
+	i = 0;
+	if (*type == 'c')
+		i += ft_putchar(va_arg(argument, int));
+	else if (*type == 's')
+		i += ft_putstr(va_arg(argument, char *));
+	else if (*type == 'd' || *type == 'i')
+		i += ft_putnbr(va_arg(argument, int));
+	else if (*type == 'p')
+		i += ft_putpointer(va_arg(argument, char *));
+	return (i);
 }
