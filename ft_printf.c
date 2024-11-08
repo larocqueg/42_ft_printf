fa815f069efee8ab4fec02c0dbbf6f9c087253a6
larocqueg <portugalgabriel2021@gmail.com>
2024-11-08 18:41:08 +0000

    test

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,17 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:21:48 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/08 18:20:08 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:36:58 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
-#include "ft_printf.h"
-#include "ft_strchr.c"
-#include "ft_putchar.c"
-#include "ft_putstr.c"
-#include "ft_putpointer.c"
+//#include "ft_printf.h"
+//#include "ft_strchr.c"
+//#include "ft_putchar.c"
+//#include "ft_putstr.c"
+//#include "ft_putpointer.c"
 
 static int	ft_putnbr(int nb);
 static int	ft_type(const char *type, va_list argument);
@@ -50,6 +50,12 @@ int	ft_printf(const char *str, ...)
 	return (i);
 }
 
+static long	ft_isneg(long n)
+{
+	ft_putchar('-');
+	return (nb * -1);
+}
+
 static int	ft_putnbr(int nb)
 {
 	char	str[11];
@@ -60,25 +66,24 @@ static int	ft_putnbr(int nb)
 	n = nb;
 	is_negative = 0;
 	if (n == 0)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
+		return (ft_putchar('0'));
 	if (n < 0)
 	{
 		is_negative = 1;
-		write(1, "-", 1);
-		n = -n;
+		n = ft_isneg(n);
 	}
 	i = 0;
 	while (n > 0)
 	{
-		str[i++] = n % 10 + '0';
+		str[i] = n % 10 + '0';
 		n /= 10;
+		i++;
 	}
-	while (i >= 0)
-		write(1, &str[i--], 1);
-	return (i + is_negative);
+	if (is_negative)
+		str[i++] = '-';
+	while (--i >= 0)
+		write(1, &str[i], 1);
+	return (i + 2 - is_negative);
 }
 
 static int	ft_type(const char *type, va_list argument)
@@ -101,6 +106,7 @@ int	main(int ac, char **av)
 {
 	int	neg = -2147483648;
 	int	pos = 2147483647;
+	int	zero = 0;
 
 	ft_printf("Here is a char: %c\n", av[1][0]);
 	printf("printf(%c)\n", av[1][0]);
@@ -108,10 +114,12 @@ int	main(int ac, char **av)
 	printf("printf(%s)\n", av[1]);
 	ft_printf("Here is a negative number: %d\n", neg);
 	printf("printf(%d)\n", neg);
+	ft_printf("Here is zero number: %d\n", zero);
 	ft_printf("Here is a positive number: %d\n", pos);
 	printf("printf(%d)\n", pos);
 	ft_printf("Here is a negative number: %i\n", neg);
 	printf("printf(%i)\n", neg);
+	ft_printf("Here is zero number: %i\n", zero);
 	ft_printf("Here is a positive number: %i\n", pos);
 	printf("printf(%i)\n", pos);
 	ft_printf("Here is the pointer of av[1]: %p\n", av[1]);
